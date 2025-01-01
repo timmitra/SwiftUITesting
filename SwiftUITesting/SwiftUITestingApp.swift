e223feeb69fb720df7281467c1135eea2a43aca8
Tim Mitra <tim@it-guy.com>
2025-01-01 14:22:49 -0500

    Initial Commit

---
+++ SwiftUITesting/SwiftUITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestingApp.swift
+//  SwiftUITesting
+//
+//  Created by Tim Mitra on 2025-01-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
